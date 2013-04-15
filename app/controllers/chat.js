12610425d0fb83b29971f8433e0a74412002f15d
Jesse Dunlap <jdunlap0@gmail.com>
2013-04-15 13:26:28 -0500

    Update chat.js
    Added the sendMessage, sendPM, and getMessage actions.

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,9 +1,83 @@
+var MessageType = {
+	PUBLIC:		1,
+	PRIVATE:	2,
+	SYSTEM:		3,
+	EMOTE:		4
+};
+
 var ChatController = function(P) {
 	this.sendMessage = function(message) {
-
-	};
-
-	this.privateMessage = function(data) {
+		var sender = P.account.username;
 		
+		P.clients.forEach(function(client) {
+			client.controller("chat").getMessage({
+				type: 		MessageType.PUBLIC,
+				sender: 	sender,
+				message:	message
+			});	
+		});
+	};
+	
+	this.sendPM = function(data) {
+		var sender    = P.account.username;
+		var recipient = data.recipient;
+		var message   = data.message;
+		
+		P.clients.forEach(function(client) {
+			if (client.account.username == recipient) {
+				client.controller("chat").getMessage({
+					type: 		MessageType.PRIVATE,
+					sender: 	sender,
+					message:	message
+				});
+			}	
+		});
+	};
+	
+	this.getMessage = function(data) {
+		switch(data.type) {
+			// Public Message
+			case 1:
+				P.$("#messages").prepend(P.snippet("chat/message", {
+					color: 		"#FFF",
+					sender: 	data.sender,
+					message: 	data.message
+				}));
+				
+				break;
+			
+			// Private Message
+			case 2:
+				P.$("#messages").prepend(P.snippet("chat/private_message", {
+					color: 		"#09F",
+					sender: 	data.sender,
+					message: 	data.message
+				}));
+				
+				break;
+				
+			// System Message
+			case 3:
+				P.$("#messages").prepend(P.snippet("chat/message", {
+					color: 		"#F00",
+					sender: 	data.sender || "System",
+					message: 	data.message
+				}));
+				
+				break;
+			
+			// Emote Message
+			case 4:
+				P.$("#messages").prepend(P.snippet("chat/emote_message", {
+					color: 		"#FFF",
+					sender: 	data.sender,
+					message: 	data.message
+				}));
+				
+				break;
+			
+			default:
+				break;
+		};
 	};
 };
